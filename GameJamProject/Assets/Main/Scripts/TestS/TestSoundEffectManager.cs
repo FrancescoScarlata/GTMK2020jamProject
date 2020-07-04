30317570ecf82df967f0a21f03e23873c11aae30
FrancescoS <francescoscarl93@gmail.com>
2020-07-04 12:44:24 +0200

    Added the scene content for the tests
    - it is missing the track and the sfx

---
+++ GameJamProject/Assets/Main/Scripts/TestS/TestSoundEffectManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSoundEffectManager : MonoBehaviour
+{
+    public AudioSource source;
+    [Header("The sfx audio clip")]
+    public AudioClip clip;
+
+    public void Start()
+    {
+        source.clip = clip;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Fire1")>0)
+        {
+            if (source.isPlaying)
+            {
+                source.Stop();
+                source.clip = clip;
+                source.Play();
+            }
+        }
+    }
+}
---
+++ GameJamProject/Assets/Main/Scripts/TestS/TestSoundTrackManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TestSoundTrackManager : MonoBehaviour
+{
+    public AudioSource source;
+    [Header("The soundtrack audio clip")]
+    public AudioClip clip;
+
+    public void Start()
+    {
+        source.clip = clip;
+        source.Play();
+    }
+
+}

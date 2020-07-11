f39d97bca3580886ed8887324a03b617d5cfe27a
Francesco Scarlata <francescoscarl93@gmail.com>
2020-07-11 18:05:16 +0200

    added the sound effect and control sprites
    - added the sound effect prefabs and scripts

---
+++ GameJamProject/Assets/Main/Scripts/Utilities/AudioSourceWithTime.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class that manage the single  audiosource for the sfx for the sound effect manager
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class AudioSourceWithTime : MonoBehaviour
+{
+    public AudioSource source;
+    [HideInInspector]
+    public bool isFinished=true;
+
+
+    public void Play(AudioClip clipsfx)
+    {
+        StartCoroutine(PlayAndWait(clipsfx));
+    }
+
+    /// <summary>
+    /// public volume that will be changed by the sound effext manager
+    /// </summary>
+    /// <param name="newValue"></param>
+    public void ChangeVolume(float newValue)
+    {
+        source.volume = newValue;
+    }
+
+
+    IEnumerator PlayAndWait(AudioClip clipSfx)
+    {
+        isFinished = false;
+        source.PlayOneShot(clipSfx);
+        yield return new WaitForSeconds(clipSfx.length);
+        isFinished = true;
+    }
+}

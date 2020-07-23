ea903123516a286093c6e263ffd87647c437db75
FrancescoS <francescoscarl93@gmail.com>
2020-07-23 10:48:41 +0200

    Starting to add a player Area Trigger
    - in the area trigger, something will happen everytime it enters or exit the trigger
    - added the tilemap package in the package manager

---
+++ GameJamProject/Assets/Main/Scripts/Utilities/AreaInteractions/AreaPortal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script manages the portals.
+/// This script has not a require component for the collider to give the freedom to add the needed collider for the case.
+/// </summary>
+public abstract class AreaPortal : MonoBehaviour
+{
+
+    [Header("select which direction the portal is passed by the player to be activated")]
+    public Direction directionOfActivation;
+
+
+    /// <summary>
+    /// Checks when a player is inside the tigger
+    /// </summary>
+    /// <param name="collision"></param>
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Activate();
+        }
+    }
+
+    /// <summary>
+    /// Checks where is the player respect the portal and the direction wanted for the activation.
+    /// </summary>
+    /// <param name="collision"></param>
+    protected virtual void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Deactivate();
+        }
+    }
+
+    /// <summary>
+    /// The method called to do something when the portal is activated
+    /// </summary>
+    protected abstract void Activate();
+
+    protected abstract void Deactivate();
+}
---
+++ GameJamProject/Assets/Main/Scripts/Utilities/AreaInteractions/InstructionsAreaPortal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class in case it is needed a trigger that gives a suggestion when it reacheas an area
+/// </summary>
+public class InstructionsAreaPortal : AreaPortal
+{
+
+    public string instructionToShow;
+
+    protected override void Activate()
+    {
+        //InGameUIManager.instance.
+    }
+
+    protected override void Deactivate()
+    {
+        
+    }
+
+}
